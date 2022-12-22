06e05a187c754ecd60ae8dd706d97e18b1742624
Samuel <samasasamu@protonmail.com>
2022-12-22 17:35:26 +0100

    Add serial comunication

--- groundst/src/main.cpp
+++ groundst/src/main.cpp
@@ -15,6 +15,13 @@
 #include <cmath>
 #include <functional>
 #include <vector>
+#include <string.h>
+
+#include <fcntl.h>
+#include <errno.h>
+#include <termios.h>
+#include <unistd.h>
+
 
 class Graph {
  public:
@@ -41,8 +48,39 @@ int main(void) {
   Graph my_graph;
   Color suscolor = Color::Red;
 
+  char serialport[] = "/dev/ttyACM0";
+  std::cout << "Enter Serial port: ";
+  std::cin >> serialport;
+  int arduino = open(serialport, O_RDWR);
+  
+  if (arduino < 0) {
+     printf("Error %i from open: %s\n", errno, strerror(errno));
+     return EXIT_FAILURE;
+  }
+
+  struct termios tty;
+
+  if(tcgetattr(arduino, &tty) != 0) {
+     printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
+     return EXIT_FAILURE;
+  }
+  
+  tty.c_lflag &= ~ISIG;
+  tty.c_oflag &= ~OPOST;
+  tty.c_oflag &= ~ONLCR;
+
+  cfsetispeed(&tty, B9600);
+  cfsetospeed(&tty, B9600);
+  
+  if (tcsetattr(arduino, TCSANOW, &tty) != 0) {
+     printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
+     return EXIT_FAILURE;
+  }
   std::string reset_position;
-  for (int i = 0;; ++i) {
+  for (int i = 0;; ++i) { 
+     char read_buf [32];
+     int n = read(arduino, &read_buf, sizeof(read_buf));
+     
      // Define the document
      auto document =
        vbox({
@@ -65,6 +103,10 @@ int main(void) {
 		text("      ▀      ") | color(suscolor),
              }) | border,
           }) | flex,
+	  hbox({
+             text(" Serial: "),
+             text(read_buf),
+          }) | flex | border | color(Color::CyanLight),
           hbox({
              text(" RSSI: "),
 	     gauge(0.75),
